8d4a2dcb2e1db8b0ea1c2642364964f2347c2af0
Colin Le <co986387@ucf.edu>
2024-11-08 16:55:27 -0500

    Update the model

--- outage_model/util/GATRNN.py
+++ outage_model/util/GATRNN.py
@@ -1,27 +1,34 @@
 import torch.nn as nn
 import torch
-from torch_geometric.nn import GATConv
-
+from torch_geometric.nn import GATConv, BatchNorm
+ 
 class GATRNN(nn.Module):
-    def __init__(self, num_static_node_features, num_static_edge_features, num_weather_features, hidden_size):
+    def __init__(self, num_static_node_features, num_static_edge_features, num_weather_features, hidden_size, lstm_hidden_size=64, num_heads=8):
         super().__init__()
-        self.gat_conv = GATConv(num_static_node_features, hidden_size, edge_dim=num_static_edge_features)
-        self.lstm = nn.LSTM(hidden_size + num_weather_features, hidden_size)
-        self.linear = nn.Linear(hidden_size, 1)
-
-    def forward(self, node_static_features, edge_static_features, node_dynamic_features, edge_index):
-        # Implement Later
-
-        time_steps = node_dynamic_features.size(1)
-
-        gat_output = self.gat_conv(node_static_features,edge_index.t(),edge_static_features)
-        gat_output = torch.relu(gat_output)
-        gat_output = gat_output.unsqueeze(1).expand(-1, time_steps, -1)
-        rnn_input = torch.cat((gat_output, node_dynamic_features), dim=2)
-        lstm_output, _ = self.lstm(rnn_input)
-        output = self.linear(lstm_output)
-
-        # Aggregate across time steps (mean pooling)
-        mean_pooled_output = torch.mean(output, dim=1)
        
-        return mean_pooled_output
+        self.gat_conv1 = GATConv(
+            num_static_node_features, hidden_size, heads=num_heads, concat=False, edge_dim=num_static_edge_features
+        )
+        self.batch_norm1 = BatchNorm(hidden_size)
+        self.lstm = nn.LSTM(input_size=num_weather_features, hidden_size=lstm_hidden_size, batch_first=True)
+       
+        combined_feature_size = hidden_size + lstm_hidden_size
+        self.hidden_linear = nn.Linear(combined_feature_size, hidden_size // 2)
+        self.output_linear = nn.Linear(hidden_size // 2, 1)
+       
+        self.dropout = nn.Dropout(0.60)
+ 
+    def forward(self, node_static_features, edge_static_features, node_dynamic_features, edge_index):
+        x1 = self.gat_conv1(node_static_features, edge_index.t(), edge_static_features)
+        x1 = torch.relu(self.batch_norm1(x1))
+        x1 = self.dropout(x1)
+       
+        lstm_out, _ = self.lstm(node_dynamic_features)
+        lstm_out = lstm_out[:, -1, :]
+       
+        combined_features = torch.cat((x1, lstm_out), dim=1)
+       
+        hidden_output = torch.relu(self.hidden_linear(combined_features))
+        output = self.output_linear(hidden_output)
+       
+        return output
